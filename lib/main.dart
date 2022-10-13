a782485de888e1bec6cd5b68118e3757dcf65894
rakeshsonkar <rksonkar356@gmail.com>
2022-10-13 12:26:56 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: avoid_unnecessary_containers
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("welcome to flutter Rakesh Sonkar"),
+          ),
+        ),
+      ),
+    );
+  }
+}
